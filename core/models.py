7fed570c7add61e073f4b83e589a8180758b493c
mateuszwwwrobel <mateusz.wwwrobel@gmail.com>
2020-09-06 14:57:31 +0100

    models added and registered

--- core/models.py
+++ core/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.conf import settings
+import datetime
 
-# Create your models here.
+#Create your models here.
+
+DAY_OF_THE_WEEK = (
+    ('Mon', 'Monday'),
+    ('Tue', 'Tuesday'),
+    ('Wed', 'Wednesday'),
+    ('Thu', 'Thursday'),
+    ('Fri', 'Friday'),
+)
+
+
+class Task(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    day_of_the_week = models.CharField(max_length=3, choices=DAY_OF_THE_WEEK)
+    duration = models.FloatField()
+    contract_number = models.CharField(max_length=5)
+    description = models.TextField()
+    car_reg = models.CharField(max_length=10, blank=True, null=True)
+
+
+    def __str__(self):
+        return self.day_of_the_week + str(self.week_ending_date.strftime(" - %d-%m-%Y"))
+
