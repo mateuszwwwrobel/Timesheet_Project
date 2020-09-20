d69212d29d7139726bf6e46b87a89c77597ab397
mateuszwwwrobel <mateusz.wwwrobel@gmail.com>
2020-09-20 09:58:19 +0100

    CSV export file added, delete job function added

--- core/views.py
+++ core/views.py
@@ -1,19 +1,24 @@
 from django.shortcuts import render, redirect
 from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, FileResponse
 from django.views.generic import View
 from .forms import AddJobForm
-from .models import Task, UserTask
+from .models import Task
 from django.contrib.auth.decorators import login_required
 from django.db.models import Sum
+import csv, datetime, io
+from reportlab.pdfgen import canvas
+from reportlab.lib.units import inch
+from django.contrib.auth.models import User
+import os
+
 
 
 # Create your views here.
 
 def landing_page_view(request):
-    
     return render(request, 'landing_page.html')
-
 
 
 class WeekView(View):
@@ -23,14 +28,11 @@ class WeekView(View):
         context = {
         'week_tasks': Task.objects.filter(user=user),
         }
-
+                
         return render(self.request, 'week_page.html', context)
-    
-    def delete_job(self):
-        user = self.request.user
 
         
-
+@login_required
 def add_job_view(request):
     user = request.user
     form = AddJobForm(request.POST or None, initial={'user': user})
@@ -42,7 +44,7 @@ def add_job_view(request):
 
     return render(request, 'add_job_form.html', context)
 
-
+@login_required
 def save_job(request):
     form = AddJobForm(request.POST or None)
     if form.is_valid():
@@ -55,38 +57,64 @@ class ResetTimesheet(View):
     def get(self, *args, **kwargs):
         user = self.request.user
         weekly_tasks = Task.objects.filter(user=user)
-        full_tasks = UserTask(weekly_tasks)
-        
-        print(full_tasks)
         weekly_tasks.delete()
 
         return render(self.request, 'reset_timesheet.html')
 
+
+""" def hello(c):
+    #Basic information about user
+    #user = Task.objects.filter(user=user)
+    user = "Matt"
+    date = str(datetime.datetime.now().day) +'.' + str(datetime.datetime.now().month) + '.' + str(datetime.datetime.now().year)
+
+    #Headline with user name and week ending date
+    c.setFont("Helvetica", 14)
+    c.drawString(30,750,"Timesheet")
+    c.setFont("Helvetica", 12)
+    c.drawString(30,770,"Week ending date   " + date)
+    c.drawString(120,750,user)
+
+    #Borders, table-like view:
+    c.translate(25,785)
+    c.rect(0,0,550,-738,1,0)
+    c.translate(107,0)
+    c.line(0,0,0,-20)
+    c.translate(-107,-20)
+    c.line(0,0,550,0)
+    c.translate(0,-20)
+    c.line(0,0,550,0) """
+
+
+@login_required
+def export_week_to_file(request):
+    user = request.user
+    response = HttpResponse(content_type='text/csv')
+    date = str(datetime.datetime.now().day) +'.' + str(datetime.datetime.now().month) + '.' + str(datetime.datetime.now().year)
+    file_name = str(user) + ' ' + str(date)
+    response['Content-Disposition'] = 'attachment; filename="Timesheet - %s.csv"' % file_name
+    file_path = os.path.join(os.path.expanduser("~"), "Desktop/", file_name)
+
+    #c = canvas.Canvas(file_path ,file_name)
+    #hello(c)
+    #c.showPage()
+    #c.save()
     
+    writer = csv.writer(response)
+    writer.writerow(['Date', 'Site name', 'Contract number', 'Hours', 'Description', 'Car registration'])
+    for task in Task.objects.filter(user=user).values_list('date', 'site_name', 'contract_number', 'duration', 'description', 'car_reg'):
+        writer.writerow(task)
 
-@login_required
-def delete_single_job_from_week(request):
-    user = request.user
-
-    try: 
-        deleted_job = Task.objects.get(user=user)
-        deleted_job.delete()
-        return redirect('core:week-page-view')
-
-    except ObjectDoesNotExist:
-
-        return redirect('core:week-page-view')
+    return response
 
 
 @login_required
-def total_hours(request):
-    user = request.user
+def delete_task(request, pk):
+    task = get_object_or_404(Task, pk=pk)
 
-    duration = Task.objects.filter(user=user)
-    try: 
-        total_h = Task.objects.get(duration=duration)
-        print(total_h)
-        return total_h
-    except ObjectDoesNotExist:
-        return redirect('core:week-page-view') 
+    if request.method == 'POST':
+        task.delete()
+        return redirect('/week/')
+
+    return redirect('/week/')
     
