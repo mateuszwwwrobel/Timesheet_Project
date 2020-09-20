3489059e09ae4658d6cbba54950108294e9b1eba
mateuszwwwrobel <mateusz.wwwrobel@gmail.com>
2020-09-20 10:32:06 +0100

    settings.py updated for heroku

--- timesheet_project/settings.py
+++ timesheet_project/settings.py
@@ -25,10 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-#['.herokuapp.com',]
+ALLOWED_HOSTS = ['.herokuapp.com',]
 
 
 # Application definition
@@ -129,8 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 
 #AUTHENTICATION: 
