31facc40d0bc90aaba579927eb02134edbae846d
mateuszwwwrobel <mateusz.wwwrobel@gmail.com>
2020-09-20 09:51:03 +0100

    UserTask model deleted,  Task model modified

--- core/admin.py
+++ core/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 
-from .models import Task,UserTask
+from .models import Task
 # Register your models here.
 
 admin.site.register(Task)
-admin.site.register(UserTask)
 
 
--- core/models.py
+++ core/models.py
@@ -1,34 +1,24 @@
 from django.db import models
 from django.conf import settings
-import datetime
-
+from django.utils import timezone 
 #Create your models here.
 
-DAY_OF_THE_WEEK = (
-    ('Mon', 'Monday'),
-    ('Tue', 'Tuesday'),
-    ('Wed', 'Wednesday'),
-    ('Thu', 'Thursday'),
-    ('Fri', 'Friday'),
-)
-
 CARS_REG = (
-    ('SM16 SWF', 'SM16 SWF - Richards (White Toyota Hilux)'),
-    ('FL64 EBN', 'FL64 EBN - Scotts (White Toyota Hilux)'),
+    ('SM16 SWF', 'SM16 SWF - White Toyota Hilux'),
+    ('FL64 EBN', 'FL64 EBN - White Toyota Hilux'),
     ('WG63 LVZ', 'WG63 LVZ - Volkswagen Caddy Van'),
     ('SC16 SYZ', 'SC16 SYZ - Vauxhall Movano Flatbed Truck'),
-    ('SA13 KLJ', 'SA13 KLJ - Daniels (White Ford Ranger'),
-    ('SJ62 RMU', 'SJ62 RMU - Matts (Black Toyota Hilux'),
-    ('HG61 HGZ', 'HG61 HGZ - Tims (Blue Toyota Hilux)'),
+    ('SA13 KLJ', 'SA13 KLJ - White Ford Ranger'),
+    ('SJ62 RMU', 'SJ62 RMU - Black Toyota Hilux'),
+    ('HG61 HGZ', 'HG61 HGZ - Blue Toyota Hilux'),
     ('SC15 MYR', 'SC15 MYR - White Land Rover Defender'),
-    ('XXXX ???', 'XXXX ??? - Alexs (Grey Toyota Hilux)'),
+    ('FG61 YVW', 'FG61 YVW - Grey Toyota Hilux'),
 )
-
 
 
 class Task(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    day_of_the_week = models.CharField(max_length=3, choices=DAY_OF_THE_WEEK)
+    date = models.DateField(default=timezone.now)
     site_name =  models.CharField(max_length=40)
     duration = models.FloatField()
     contract_number = models.CharField(max_length=5, blank=True, null=True)
@@ -37,9 +27,7 @@ class Task(models.Model):
 
 
     def __str__(self):
-        return str(self.user) +' - ' + self.day_of_the_week
+        return str(self.user) +' - ' + str(self.date)
 
 
 
-class UserTask(models.Model):
-    all_tasks = models.ForeignKey(Task, on_delete=models.CASCADE)
